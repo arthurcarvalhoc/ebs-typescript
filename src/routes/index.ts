42d1a38ecec45a03e663b99659536dd4db0afb5b
Arthur Carvalho <arthurcarvalhoc@gmail.com>
2023-09-14 15:49:05 -0300

    Adding watch mode

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,7 +5,7 @@ export const register = (app: express.Application) => {
     // home page
     app.get("/", (req: any, res) => {
         res.render("index", {
-            data: "teste"
+            data: "test"
         });
     });
 
